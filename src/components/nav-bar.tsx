058a22c0cf1091e11519ca5d172897213e867236
laviniapark <laviniasoo03@gmail.com>
2025-03-26 19:40:42 -0300

    Ajuste de cores da barra de navegacao

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -13,10 +13,10 @@ const links = [
 
 export default function NavBar(props: NavBarProps) {
     const { active } = props;
-    const classActive = "border-b-4 border-teal-300 bg-indigo-950"; // Apenas a borda inferior
+    const classActive = "border-b-4 border-[#1CCAD8] bg-[#0B132B]"; // Apenas a borda inferior
 
     return (
-        <nav className="flex justify-between bg-blue-950 px-6 h-18 items-center">
+        <nav className="flex justify-between bg-[#1C2541] px-6 h-18 items-center">
             <ul className="flex gap-12 text-2xl mx-auto h-full items-center">
                 {links.map(link => (
                 <li key={link.label} className={`h-full flex items-end pb-2 ${active === link.label ? classActive : ""}`}>
