079b565e6af3fc997253243e271a2696f6c0a15f
giovanna2005lb <gikalaturague@gmail.com>
2025-03-27 00:52:10 -0300

    Iniciei a pagina Criações

--- src/app/creations/page.tsx
+++ src/app/creations/page.tsx
@@ -1,9 +1,19 @@
+"use client";
+
 import NavBar from "@/components/nav-bar";
+import {Button} from "@heroui/react";
 
 export default function ManagerCharacterPage(){
     return (
         <>
-            <NavBar active="Criações"/>
+            <div>
+                <NavBar active="Criações"/>
+                <main className="flex justify-center gap-10">
+                    <Button className="bg-[#091A09] hover:bg-[#0c2b0c] border-4 border-[#116A71] rounded-2xl flex justify-center items-center text-2xl h-20 w-40">
+                    Adicionar
+                    </Button>
+                </main>
+            </div>
         </>
     )
 }
