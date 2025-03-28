fde98bf9f5b2b567c4aea55c8df79eab36a17204
laviniapark <laviniasoo03@gmail.com>
2025-03-28 04:11:06 -0300

    Alteração das fichas da pagina inicial

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,6 +4,7 @@ import NavBar from "@/components/nav-bar";
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
 import Link from 'next/link';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function HomePage(){
     return (
@@ -53,26 +54,103 @@ export default function HomePage(){
                         width={50}
                         height={50}
                     />
+                    <div className="w-full flex items-center justify-center mt-10">
+                        <Link href="/creations">
+                            <Button className="bg-[#1C2541] border-4 border-[#1CCAD8] rounded-lg text-5xl py-8 px-15 hover:opacity-90 
+                        transition-opacity duration-500 ease-in-out hover:cursor-pointer">
+                            <span className="text-[#1CCAD8]">START</span></Button>
+                        </Link>
+                    </div>
+
+                    <div className="w-full mt-25 flex gap-10 justify-center">
+                        <Card className="flex flex-col gap-5 w-80 h-90 bg-[#1C2541] border-2 border-[#5f6880] items-center">
+                            <CardHeader className="flex items-center w-full pl-2">
+                                <Image
+                                    className="rounded-full"
+                                    src="/default-icon.jpeg"
+                                    alt="imagem padrao"
+                                    width={70}
+                                    height={70}
+                                />
+                                <div className="flex flex-col justify-center pl-4">
+                                    <CardTitle className="text-3xl text-accent">João Carlos</CardTitle>
+                                    <CardDescription className="text-xl text-[#858995]">Idade: ??</CardDescription>
+                                </div>
+                            </CardHeader>
+                            {/* DIV DO CONTEÚDO DO CARD */}
+                            <div className="w-75 h-full bg-[#2d3757] rounded-sm p-0">
+                                <CardContent className="flex flex-col gap-0 grow p-2">
+                                    <p className="text-accent font-bold text-lg">Descrição</p>
+                                    <CardDescription className="break-words text-gray-400">
+                                        Brincalhão e extrovertido
+                                    </CardDescription>
+                                    <p className="text-accent font-bold text-lg pt-1">Poderes</p>
+                                    <CardDescription className="break-words text-gray-400">
+                                        Ser engraçado
+                                    </CardDescription>
+                                </CardContent>
+                            </div>
+                        </Card>
+
+                        <Card className="flex flex-col gap-5 w-80 h-90 bg-[#1C2541] border-2 border-[#5f6880] items-center">
+                            <CardHeader className="flex items-center w-full pl-2">
+                                <Image
+                                    className="rounded-full"
+                                    src="/default-icon.jpeg"
+                                    alt="imagem padrao"
+                                    width={70}
+                                    height={70}
+                                />
+                                <div className="flex flex-col justify-center pl-4">
+                                    <CardTitle className="text-3xl text-accent">Giovanna Laturague</CardTitle>
+                                    <CardDescription className="text-xl text-[#858995]">Idade: 19</CardDescription>
+                                </div>
+                            </CardHeader>
+                            {/* DIV DO CONTEÚDO DO CARD */}
+                            <div className="w-75 h-full bg-[#2d3757] rounded-sm p-0">
+                                <CardContent className="flex flex-col gap-0 grow p-2">
+                                    <p className="text-accent font-bold text-lg">Descrição</p>
+                                    <CardDescription className="break-words text-gray-400">
+                                        Super gente fina e ama bater um papo
+                                    </CardDescription>
+                                    <p className="text-accent font-bold text-lg pt-1">Poderes</p>
+                                    <CardDescription className="break-words text-gray-400">
+                                        Excelente em jogos
+                                    </CardDescription>
+                                </CardContent>
+                            </div>
+                        </Card>
+
+                        <Card className="flex flex-col gap-5 w-80 h-90 bg-[#1C2541] border-2 border-[#5f6880] items-center">
+                            <CardHeader className="flex items-center w-full pl-2">
+                                <Image
+                                    className="rounded-full"
+                                    src="/default-icon.jpeg"
+                                    alt="imagem padrao"
+                                    width={70}
+                                    height={70}
+                                />
+                                <div className="flex flex-col justify-center pl-4">
+                                    <CardTitle className="text-3xl text-accent">Lavinia Park</CardTitle>
+                                    <CardDescription className="text-xl text-[#858995]">Idade: 21</CardDescription>
+                                </div>
+                            </CardHeader>
+                            {/* DIV DO CONTEÚDO DO CARD */}
+                            <div className="w-75 h-full bg-[#2d3757] rounded-sm p-0">
+                                <CardContent className="flex flex-col gap-0 grow p-2">
+                                    <p className="text-accent font-bold text-lg">Descrição</p>
+                                    <CardDescription className="break-words text-gray-400">
+                                        Complicada de lidar, porém ama um rolezinho insalubre
+                                    </CardDescription>
+                                    <p className="text-accent font-bold text-lg pt-1">Poderes</p>
+                                    <CardDescription className="break-words text-gray-400">
+                                        Sabe dançar macarena de cor
+                                    </CardDescription>
+                                </CardContent>
+                            </div>
+                        </Card>
+                    </div>
                     
-                    <Link href="/creations" className="w-full flex items-center justify-center mt-10">
-                        <Button
-                        className="absolute text-5xl py-10 px-30
-                            bg-gradient-to-br from-[#1CCAD8] to-[#03383c]
-                            text-white shadow-lg rounded-lg">
-                        </Button>
-
-                        <button className="relative bg-[#1C2541] rounded-lg text-5xl py-3 px-15 hover:opacity-90 
-                    transition-opacity duration-500 ease-in-out">
-                        <span className="text-[#1CCAD8]">START</span></button>
-                    </Link>
-
-                    <Image
-                        className="mt-20"
-                        src="/form-examples.png"
-                        alt="Imagem exemplo de formularios"
-                        width={1100}
-                        height={1100}
-                    />
             </main>
         </div>
         </>
