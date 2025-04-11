5b9aea93dc96f425056e17d0fafc595275a1cb2a
laviniapark <laviniasoo03@gmail.com>
2025-04-11 01:17:40 -0300

    Correçao de codigo duplicado

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,9 +5,25 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button"
 import Link from 'next/link';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import CharacterCard from "@/components/character-card";
+import { useEffect, useState } from "react";
+import { Character } from "@/lib/character";
 
 export default function HomePage(){
+    
+    const [characters, setCharacters] = useState<Character[]>([]); 
+    
+    useEffect(() => {
+        async function getCharacters() {
+            const response = await fetch("http://localhost:8080/home");
+            const data =  await response.json();
+            setCharacters(data);
+        }
+        getCharacters();
+    }, []);
+
     return (
+
         <>
         <div className="flex flex-col min-h-screen">
             
@@ -63,92 +79,9 @@ export default function HomePage(){
                     </div>
 
                     <div className="w-full mt-25 flex gap-10 justify-center">
-                        <Card className="flex flex-col gap-5 w-80 h-90 bg-[#1C2541] border-2 border-[#5f6880] items-center">
-                            <CardHeader className="flex items-center w-full pl-2">
-                                <Image
-                                    className="rounded-full"
-                                    src="/default-icon.jpeg"
-                                    alt="imagem padrao"
-                                    width={70}
-                                    height={70}
-                                />
-                                <div className="flex flex-col justify-center pl-4">
-                                    <CardTitle className="text-3xl text-accent">João Carlos</CardTitle>
-                                    <CardDescription className="text-xl text-[#858995]">Idade: ??</CardDescription>
-                                </div>
-                            </CardHeader>
-                            {/* DIV DO CONTEÚDO DO CARD */}
-                            <div className="w-75 h-full bg-[#2d3757] rounded-sm p-0">
-                                <CardContent className="flex flex-col gap-0 grow p-2">
-                                    <p className="text-accent font-bold text-lg">Descrição</p>
-                                    <CardDescription className="break-words text-gray-400">
-                                        Brincalhão e extrovertido
-                                    </CardDescription>
-                                    <p className="text-accent font-bold text-lg pt-1">Poderes</p>
-                                    <CardDescription className="break-words text-gray-400">
-                                        Ser engraçado
-                                    </CardDescription>
-                                </CardContent>
-                            </div>
-                        </Card>
-
-                        <Card className="flex flex-col gap-5 w-80 h-90 bg-[#1C2541] border-2 border-[#5f6880] items-center">
-                            <CardHeader className="flex items-center w-full pl-2">
-                                <Image
-                                    className="rounded-full"
-                                    src="/default-icon.jpeg"
-                                    alt="imagem padrao"
-                                    width={70}
-                                    height={70}
-                                />
-                                <div className="flex flex-col justify-center pl-4">
-                                    <CardTitle className="text-3xl text-accent">Giovanna Laturague</CardTitle>
-                                    <CardDescription className="text-xl text-[#858995]">Idade: 19</CardDescription>
-                                </div>
-                            </CardHeader>
-                            {/* DIV DO CONTEÚDO DO CARD */}
-                            <div className="w-75 h-full bg-[#2d3757] rounded-sm p-0">
-                                <CardContent className="flex flex-col gap-0 grow p-2">
-                                    <p className="text-accent font-bold text-lg">Descrição</p>
-                                    <CardDescription className="break-words text-gray-400">
-                                        Super gente fina e ama bater um papo
-                                    </CardDescription>
-                                    <p className="text-accent font-bold text-lg pt-1">Poderes</p>
-                                    <CardDescription className="break-words text-gray-400">
-                                        Excelente em jogos
-                                    </CardDescription>
-                                </CardContent>
-                            </div>
-                        </Card>
-
-                        <Card className="flex flex-col gap-5 w-80 h-90 bg-[#1C2541] border-2 border-[#5f6880] items-center">
-                            <CardHeader className="flex items-center w-full pl-2">
-                                <Image
-                                    className="rounded-full"
-                                    src="/default-icon.jpeg"
-                                    alt="imagem padrao"
-                                    width={70}
-                                    height={70}
-                                />
-                                <div className="flex flex-col justify-center pl-4">
-                                    <CardTitle className="text-3xl text-accent">Lavinia Park</CardTitle>
-                                    <CardDescription className="text-xl text-[#858995]">Idade: 21</CardDescription>
-                                </div>
-                            </CardHeader>
-                            {/* DIV DO CONTEÚDO DO CARD */}
-                            <div className="w-75 h-full bg-[#2d3757] rounded-sm p-0">
-                                <CardContent className="flex flex-col gap-0 grow p-2">
-                                    <p className="text-accent font-bold text-lg">Descrição</p>
-                                    <CardDescription className="break-words text-gray-400">
-                                        Complicada de lidar, porém ama um rolezinho insalubre
-                                    </CardDescription>
-                                    <p className="text-accent font-bold text-lg pt-1">Poderes</p>
-                                    <CardDescription className="break-words text-gray-400">
-                                        Sabe dançar macarena de cor
-                                    </CardDescription>
-                                </CardContent>
-                            </div>
-                        </Card>
+                        {characters.map((character) => (
+                        <CharacterCard key={character.id} character={character} />
+                        ))}
                     </div>
                     
             </main>
